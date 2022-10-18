e2f80578118c34eee77f729966ffcb31ba1dde9b
Meazi <meazimekonnen93@gmail.com>
2022-10-18 13:38:15 -0700

    major change

--- _printf.c
+++ _printf.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include <stdarg.h>
 
 /**
  * _printf - function prints output on screen
@@ -8,43 +7,56 @@
  */
 int _printf(const char *format, ...)
 {
-	const char *character;
-	va_list args;
-	int count = 0;
+	int i, printed = 0, printed_chars = 0;
+	int flags, width, precision, size, buff_ind = 0;
+	va_list list;
+	char buffer[BUFF_SIZE];
 
-	va_start(args, format);
-	if (!format || (format[0] == '%' && !format[1]))
+	if (format == NULL)
 		return (-1);
-	if (format[0] == '%' && format[1] == ' ' && !format[2])
-		return (-1);
-	for (character = format; *character != '\0'; character++)
+
+	va_start(list, format);
+
+	for (i = 0; format && format[i] != '\0'; i++)
 	{
-		if (*character != '%')
+		if (format[i] != '%')
 		{
-			_putchar(*character);
-				count++;
+			buffer[buff_ind++] = format[i];
+			if (buff_ind == BUFF_SIZE)
+				print_buffer(buffer, &buff_ind);
+			printed_chars++;
 		}
 		else
 		{
-			character++;
-			switch (*character)
-			{
-			case 'c':
-				_putchar(va_arg(args, int));
-				count++;
-				break;
-			case 's':
-				count = count + _puts(va_arg(args, const char *));
-				break;
-			case '%':
-				_putchar('%');
-				count++;
-				break;
-			default:
+			print_buffer(buffer, &buff_ind);
+			flags = get_flags(format, &i);
+			width = get_width(format, &i, list);
+			precision = get_precision(format, &i, list);
+			size = get_size(format, &i);
+			++i;
+			printed = handle_print(format, &i, list, buffer,
+					flags, width, precision, size);
+			if (printed == -1)
 				return (-1);
-			}
+			printed_chars += printed;
 		}
 	}
-	va_end(args);
-	return (count);
+	print_buffer(buffer, &buff_ind);
+
+	va_end(list);
+
+	return (printed_chars);
+}
+
+/**
+ * print_buffer - Prints characters of the content if it exists
+ * @buffer: array of characters
+ * @buff_ind: index
+ */
+void print_buffer(char buffer[], int *buff_ind)
+{
+	if (*buff_ind > 0)
+		write(1, &buffer[0], *buff_ind);
+
+	*buff_ind = 0;
 }
