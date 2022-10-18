f79bdba9331afbaa119b48549b8e24cdda24b9ff
Meazi <meazimekonnen93@gmail.com>
2022-10-18 06:28:47 -0700

    betty

--- _printf.c
+++ _printf.c
@@ -3,39 +3,38 @@
 
 /**
  * _printf - function prints output on screen
- * @fomrat: pointer to the first part of printf
+ * @format: pointer to the first part of printf
  * Return: 0
- */ 
+ */
 int _printf(const char *format, ...)
 {
 	const char *character;
 	int i;
 	const char *s;
-	
-
 	va_list args;
+
 	va_start(args, format);
 
-	for (character = format; *character !='\0'; character++)
+	for (character = format; *character != '\0'; character++)
 	{
 		if (*character != '%')
 			_putchar(*character);
-		
 		else
 		{
 			character++;
 
 			switch (*character)
 			{
-			case 'c' : i = va_arg(args, int);
+			case 'c':
+				i = va_arg(args, int);
 				   _putchar(i);
-				   break;
-			case 's': s = va_arg(args, const char *);
+				break;
+			case 's':
+				s = va_arg(args, const char *);
 				 _puts(s);
-				  break;
+				break;
 			}
 		}
 	}
 	return (0);
-	
 }
--- _puts.c
+++ _puts.c
@@ -1,5 +1,10 @@
 #include "main.h"
 #include <stdio.h>
+/**
+ * _puts - prints a string on a standard screen
+ * @name: parameter passed from _printf
+ * Return: 1 if successful and -1 if not
+ */
 int _puts(const char *name)
 {
 	if (name == NULL)
