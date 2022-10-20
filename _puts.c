288d39f445bfdba7506819f28b112dee290d18e7
Haileab Wolde <womahaileab1428@gmail.com>
2022-10-20 05:23:27 -0700

    _putchar

--- _puts.c
+++ _puts.c
@@ -3,6 +3,8 @@
 /**
  * _puts - prints a string to stdout
  * @str: pointer to the string to print
+ *
+ * Description: a putchar inorder to print a char function
  * Return: number of chars written
  */
 int _puts(char *str)
